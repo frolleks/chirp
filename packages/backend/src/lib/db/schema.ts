caa12e579ff8669081847b80987b8e04c92d15fa
frolleks <frolleks@hotmail.com>
2024-06-20 12:13:05 +0700

    initial commit

---
+++ packages/backend/src/lib/db/schema.ts
@@ -0,0 +1,33 @@
+import { createId } from "@paralleldrive/cuid2";
+import { relations } from "drizzle-orm";
+import { pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  username: varchar("username", { length: 32 }).unique().notNull(),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdateFn(() => new Date()),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
+
+export const posts = pgTable("posts", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  content: text("content").notNull(),
+  authorId: text("author_id"),
+  createdAt: timestamp("created_at").defaultNow(),
+  updatedAt: timestamp("updated_at").$onUpdateFn(() => new Date()),
+});
+
+export const postsRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
