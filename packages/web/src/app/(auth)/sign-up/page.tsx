a4e0cdaf97ae9bf88aa766aded47e32b418fa0ea
frolleks <frolleks@hotmail.com>
2024-06-21 16:29:37 +0700

    feat(web): add sign up form

--- packages/web/src/app/(auth)/sign-up/page.tsx
+++ packages/web/src/app/(auth)/sign-up/page.tsx
@@ -1,3 +1,9 @@
+import { SignUpForm } from "@/components/sign-up-form";
+
 export default function SignUp() {
-  return <></>;
+  return (
+    <>
+      <SignUpForm />
+    </>
+  );
 }
---
+++ packages/web/src/components/sign-up-form.tsx
@@ -0,0 +1,106 @@
+"use client";
+
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  username: z.string().max(64),
+  email: z.string().email(),
+  password: z.string().min(8).max(128),
+});
+
+export function SignUpForm() {
+  const [step, setStep] = useState(1);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        {step === 1 && (
+          <>
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="frolleks" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button onClick={nextStep}>Next</Button>
+          </>
+        )}
+
+        {step === 2 && (
+          <>
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="frolleks@example.com"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your password..."
+                      type="password"
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <Button onClick={prevStep}>Back</Button>
+            <Button type="submit">Submit</Button>
+          </>
+        )}
+      </form>
+    </Form>
+  );
+}
