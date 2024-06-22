35108f075270862be0d1e6e4e2fc8fe58ecbee9d
frolleks <frolleks@hotmail.com>
2024-06-22 16:27:22 +0700

    feat(web): add posts to timeline

--- packages/web/src/components/home-timeline.tsx
+++ packages/web/src/components/home-timeline.tsx
@@ -1,14 +1,36 @@
+"use client";
+
+import useSWR from "swr";
 import { HomePostForm } from "./home-post-form";
+import { fetcher } from "@/lib/utils";
+import { assert } from "@sindresorhus/is";
+import { Post } from "./post";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export function HomeTimeline() {
+  const { data, isLoading, error } = useSWR("/api/v1/posts?max=50", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  assert.array(data);
+
   return (
-    <div className="border h-screen">
-      <div className="flex items-center w-[38rem] p-2 border-b">
+    <div className="border h-screen w-[38rem] flex flex-col">
+      <div className="flex items-center p-2 border-b">
         <div className="w-full">
           <HomePostForm />
         </div>
-        <div></div>
       </div>
+      <ScrollArea className="flex-1 overflow-y-auto">
+        {data
+          .sort(
+            (a: any, b: any) => new Date(b.createdAt) - new Date(a.createdAt)
+          ) // Replace 'createdAt' with your timestamp field
+          .map((post: any) => (
+            <Post key={post.id} post={post} />
+          ))}
+      </ScrollArea>
     </div>
   );
 }
