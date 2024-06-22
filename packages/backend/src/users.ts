9ce0e91fea1d2f4b34475347f8957998268b2cfb
frolleks <frolleks@hotmail.com>
2024-06-22 08:49:02 +0700

    feat(backend): add trycatch for the routes

--- packages/backend/src/users.ts
+++ packages/backend/src/users.ts
@@ -6,32 +6,46 @@ import { eq } from "drizzle-orm";
 export const users = new Hono();
 
 users.get("/:id", async (c) => {
-  const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-  const user = await db.query.users.findFirst({
-    where: eq(usersTable.id, id),
-    with: {
-      posts: true,
-    },
-  });
+    const user = await db.query.users.findFirst({
+      where: eq(usersTable.id, id) || eq(usersTable.username, id),
+      with: {
+        posts: true,
+      },
+      columns: {
+        id: true,
+        username: true,
+        createdAt: true,
+      },
+    });
 
-  if (!user) {
-    return c.status(404);
+    if (!user) {
+      return c.status(404);
+    }
+
+    return c.json(user);
+  } catch (error) {
+    console.error(error);
+    return c.status(500);
   }
-
-  return c.json(user);
 });
 
 users.get("/:id/posts", async (c) => {
-  const id = c.req.param("id");
+  try {
+    const id = c.req.param("id");
 
-  const posts = await db.query.posts.findMany({
-    where: eq(postsTable.authorId, id),
-  });
+    const posts = await db.query.posts.findMany({
+      where: eq(postsTable.authorId, id),
+    });
 
-  if (!posts) {
-    return c.status(404);
+    if (!posts) {
+      return c.status(404);
+    }
+
+    return c.json(posts);
+  } catch (error) {
+    return c.status(500);
   }
-
-  return c.json(posts);
 });
