6eae8766ab30a4ec343ebccf327f6dd4303ced34
frolleks <frolleks@hotmail.com>
2024-06-22 08:49:45 +0700

    refactor(web): make layout use flex

--- packages/web/src/app/(home)/layout.tsx
+++ packages/web/src/app/(home)/layout.tsx
@@ -6,9 +6,9 @@ export default function HomeLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <div className="flex">
       <HomeNavbar />
       {children}
-    </>
+    </div>
   );
 }
