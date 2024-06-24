95e5638d8a763f23f09aeb1cc1ac25dbe571db37
frolleks <frolleks@hotmail.com>
2024-06-24 15:48:50 +0700

    feat(web): add ability to go to a user's profile by clicking their username

--- packages/web/src/components/post.tsx
+++ packages/web/src/components/post.tsx
@@ -1,11 +1,16 @@
 import { UserCircleIcon } from "lucide-react";
+import Link from "next/link";
 
 export function Post({ post }: { post: any }) {
   return (
     <div className="flex items-center gap-1.5 border-b px-2 py-1.5">
       <UserCircleIcon size={36} />
-      <div className="w-full">
-        <p className="text-sm font-semibold">{post.author.username}</p>
+      <div>
+        <Link href={`/users/${post.author.username}`}>
+          <p className="text-sm font-semibold hover:underline">
+            {post.author.username}
+          </p>
+        </Link>
         <p className="text-sm break-words break-all">{post.content}</p>
       </div>
     </div>
